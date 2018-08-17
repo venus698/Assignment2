d91b3c23e2cc0df0b05e2fa46da544ffa50ddd44
venus698 <venusgoyal98@gmail.com>
2018-08-17 22:01:54 +0530

    assignment

---
+++ using_placeholders.py
@@ -0,0 +1,9 @@
+s=" Acadeview "
+course=" Python "
+fees= 5000 
+s1="%s is an online platform"
+course1="%s is a programing language"
+fees1="%d is the fees for course"
+print(s1%s)
+print(course1%course)
+print(fees1%fees)
